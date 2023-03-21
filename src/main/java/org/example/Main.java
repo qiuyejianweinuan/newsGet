82540c38b47967aa19c6ba9729f09dceb2887940
Yangxinyu <782657849@qq.com>
2023-03-21 23:45:19 +0800

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;/**
+    *@author yangxinyu
+    *@date ${DATE} ${TIME}
+**/public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/org/example/News.java
@@ -0,0 +1,5 @@
+package org.example;/**
+    *@author yangxinyu
+    *@date 2023/1/7 23:20
+**/public class News {
+}
